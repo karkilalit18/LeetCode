4716e38586682bc5551ef75797392f3b56f43cab
Lalit Singh Karki <karkilalit18@gmail.com>
2026-07-06 21:39:01 +0530

    Time: 12 ms (88.98%), Space: 12.4 MB (13.78%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        temp = abs(x)
+        result = 0
+        while temp > 0:
+            result = result * 10 + temp % 10
+            temp = temp // 10
+        if result > (2**31-1) or result < -2**31:
+            return 0
+        else:
+            if x > 0:
+                return result
+            else:
+                return -result
