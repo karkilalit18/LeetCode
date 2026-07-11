530104455a42f81699c38e10df4f340d81f1028a
Lalit Singh Karki <karkilalit18@gmail.com>
2026-07-11 11:38:00 +0530

    Time: 7 ms (13.9%), Space: 12.9 MB (7.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        st =[]
+
+        for i in range (len(s)):
+            if st :
+                last = st[-1]
+                if self.is_pair(last , s[i]):
+                    st.pop()
+                    continue
+            st.append(s[i])
+        
+        return not st
+
+    def is_pair (self , last , cur):
+        if last == "(" and cur == ")" or last =="{" and cur == "}" or last == "[" and cur == "]":
+            return True
+
+        return False
